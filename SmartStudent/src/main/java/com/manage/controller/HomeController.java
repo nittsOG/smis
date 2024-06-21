e2cb74800bd724bf4b965c26baef2f8f0e057c59
nittsOG <naiti@LAPTOP-0GTDC3PJ>
2024-06-21 19:32:41 +0530

    working project setup completed

---
+++ SmartStudent/src/main/java/com/manage/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@RequestMapping
+	public String home() {
+		// this will returnname of calling file that will paste in controller.xml in as >> /WEB-INF/views/ index.jsp
+		System.out.print("this is home or index page url");  
+		return "index";
+	}
+}
